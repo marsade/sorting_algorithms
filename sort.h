8ceb4f60ff2f71b7d1bf977e33bcaeaef6bcaae1
marsade <adenijifara@gmail.com>
2023-10-24 16:43:15 +0300

    add swap prototype

--- sort.h
+++ sort.h
@@ -20,6 +20,7 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void swap(listint_t **h, listint_t **n1, listint_t *n2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 #endif
