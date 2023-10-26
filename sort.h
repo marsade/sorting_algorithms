f4e403c59559f184b09616c60ac9fd65f7b782c8
marsade <adenijifara@gmail.com>
2023-10-26 11:48:56 +0300

    add shell_sort function prototype

--- sort.h
+++ sort.h
@@ -26,4 +26,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *arr, int low, int high, size_t size);
 int lomuto_partition(int *arr, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
